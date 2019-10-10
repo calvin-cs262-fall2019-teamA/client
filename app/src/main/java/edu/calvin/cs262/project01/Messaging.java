a018d72b9db87291de11ec75e7852a219e8859b2
nrs32 <38587739+nrs32@users.noreply.github.com>
2019-10-10 13:33:39 -0400

    fixes from merge

---
+++ app/src/main/java/edu/calvin/cs262/project01/Messaging.java
@@ -0,0 +1,29 @@
+package edu.calvin.cs262.project01;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+
+public class Messaging extends Activity {
+
+    private String channelID = "csgk6hEIPp8ZKg0K";
+    private String roomName = "observable-room";
+    private EditText editText;
+    private Button Messaging;
+
+    public static void setOnClickListener(View.OnClickListener onClickListener) {
+    }
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_messaging_page_counselor);
+        getIntent();
+
+        //This is where we write the message
+        editText = (EditText) findViewById(R.id.editText);
+    }
+}
