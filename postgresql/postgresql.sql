39f4183aa28e95483ec837de1d405dec62402f7a
stz4 <stz4@gold01.cs.calvin.edu>
2019-10-29 14:56:16 -0400

    updates postgresql

--- postgresql/postgresql.sql
+++ postgresql/postgresql.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS appointment;
 DROP TABLE IF EXISTS groupEvent;
 DROP TABLE IF EXISTS personGroupChatLink;
 DROP TABLE IF EXISTS groupChat;
-DROP TABLE IF EXISTS patient;
-DROP TABLE IF EXISTS listener;
 DROP TABLE IF EXISTS Person;
 
 --create a table for person
@@ -19,16 +17,8 @@ CREATE TABLE Person(
 		profPicture bytea,
 		showPicture boolean,
 		phoneNum varchar(15) NOT NULL,
-		showPhoneNum boolean);
-
---create a listener 
-CREATE TABLE listener(
-		personID integer REFERENCES Person(ID),
-		iscounselor boolean); --to check if they are peer or conselor
-
--- create a patient
-CREATE TABLE patient(
-		personID integer REFERENCES Person(ID));
+		showPhoneNum boolean,
+		iscounselor boolean);
 
 --create a group chat
 CREATE TABLE groupChat(
@@ -55,7 +45,8 @@ CREATE TABLE groupEvent(
 --crate an appointment 
 CREATE TABLE appointment(
 		ID integer PRIMARY KEY,
-		personID integer REFERENCES Person(ID),
+		patientID integer REFERENCES Person(ID) NOT NULL,
+		counselorID integer REFERENCES Person(ID) NOT NULL,
 		location varchar(50) NOT NULL,
 		aDate date NOT NULL,
 		aTime time NOT NULL
@@ -63,34 +54,19 @@ CREATE TABLE appointment(
 
 --allow users to select data from table
 GRANT SELECT ON Person TO PUBLIC;
-GRANT SELECT ON listener TO PUBLIC;
-GRANT SELECT ON patient TO PUBLIC;
 GRANT SELECT ON groupChat TO PUBLIC;
 GRANT SELECT ON personGroupChatLink TO PUBLIC;
 GRANT SELECT ON groupEvent TO PUBLIC;
 GRANT SELECT ON appointment TO PUBLIC;
 
 -- insert to person: ID, email, name, password, showName, profleDescription, profPic, showPic, phoneNum, showPhoneNum
-INSERT INTO Person VALUES(1, 'sam@gmail.com', 'Sam', '12345', FALSE, 'I want to talk to people', 'profpic.png',  FALSE, '616-616-6161', FALSE); 
-INSERT INTO Person VALUES(2, 'sambridhi@gmail.com', 'Sebrina', '12345', TRUE, 'I want to fo to group ebents', 'profpic.png',  FALSE, '616-616-6161', FALSE); 
-INSERT INTO Person VALUES(3, 'samsam@gmail.com', 'Hellen', '12345', FALSE, 'I am not sure', 'profpic.png',  TRUE, '616-555-6161', TRUE); 
-INSERT INTO Person VALUES(4, 'sam123@gmail.com', 'John', '12345', TRUE, 'I like having conversation', 'profpic.png',  TRUE, '616-345-6161', FALSE); 
-INSERT INTO Person VALUES(5, 'sam321@gmail.com', 'Jonhnathan', '12345', FALSE, 'I want to talk to people', 'profpic.png',  FALSE, '616-754-6161', FALSE); 
-INSERT INTO Person VALUES(6, 'sam11@gmail.com', 'Vanderbee', '12345', TRUE, 'No description', 'profpic.png',  TRUE, '616-235-6161', TRUE); 
-INSERT INTO Person VALUES(7, 'sam10@gmail.com', 'Devries', '12345', FALSE, NULL , 'profpic.png',  FALSE, '616-754-6161', FALSE); 
-
--- insert into listener: personID, iscounselor
-INSERT INTO listener VALUES(1, TRUE);
-INSERT INTO listener VALUES(2, FALSE);
-INSERT INTO listener VALUES(3, FALSE);
-INSERT INTO listener VALUES(3, FALSE);
-INSERT INTO listener VALUES(3, FALSE);
-
---insert into patient: personID
-INSERT INTO patient VALUES(3);
-INSERT INTO patient VALUES(4);
-INSERT INTO patient VALUES(5);
-INSERT INTO patient VALUES(6);
+INSERT INTO Person VALUES(1, 'sam@gmail.com', 'Sam', '12345', FALSE, 'I want to talk to people', 'profpic.png',  FALSE, '616-616-6161', FALSE,TRUE); 
+INSERT INTO Person VALUES(2, 'sambridhi@gmail.com', 'Sebrina', '12345', TRUE, 'I want to fo to group ebents', 'profpic.png',  FALSE, '616-616-6161', FALSE,TRUE); 
+INSERT INTO Person VALUES(3, 'samsam@gmail.com', 'Hellen', '12345', FALSE, 'I am not sure', 'profpic.png',  TRUE, '616-555-6161', TRUE, FALSE); 
+INSERT INTO Person VALUES(4, 'sam123@gmail.com', 'John', '12345', TRUE, 'I like having conversation', 'profpic.png',  TRUE, '616-345-6161', FALSE, FALSE); 
+INSERT INTO Person VALUES(5, 'sam321@gmail.com', 'Jonhnathan', '12345', FALSE, 'I want to talk to people', 'profpic.png',  FALSE, '616-754-6161', FALSE, TRUE); 
+INSERT INTO Person VALUES(6, 'sam11@gmail.com', 'Vanderbee', '12345', TRUE, 'No description', 'profpic.png',  TRUE, '616-235-6161', TRUE, FALSE); 
+INSERT INTO Person VALUES(7, 'sam10@gmail.com', 'Devries', '12345', FALSE, NULL , 'profpic.png',  FALSE, '616-754-6161', FALSE, FALSE); 
 
 --insert into group chat: ID, isPublic, name, description
 INSERT INTO groupChat VALUES(1, TRUE, 'Chat1', 'I am good at listening.');
@@ -111,10 +87,10 @@ INSERT INTO groupEvent VALUES(3,'John', 'An event for college students','SC 331'
 INSERT INTO groupEvent VALUES(4,'Vanderbee', 'An event everybody','SC221', '1/1/2000', '4:00:00');
 
 --insert into appointment: ID, personID, location, aDate, aTime
-INSERT INTO appointment VALUES(1,2, 'SC221', '12/12/2000', '1:00:00');
-INSERT INTO appointment VALUES(2,3, 'SC331', '1/12/2000', '2:00:00');
-INSERT INTO appointment VALUES(3,4, 'SC331', '1/12/2000', '3:00:00');
-INSERT INTO appointment VALUES(4,1, 'SC220', '5/12/2000', '4:00:00');
+INSERT INTO appointment VALUES(1,3,1, 'SC221', '12/12/2000', '1:00:00');
+INSERT INTO appointment VALUES(2,3,2, 'SC331', '1/12/2000', '2:00:00');
+INSERT INTO appointment VALUES(3,6,2, 'SC331', '1/12/2000', '3:00:00');
+INSERT INTO appointment VALUES(4,7,1, 'SC220', '5/12/2000', '4:00:00');
 
 SELECT name FROM Person
 WHERE showName = TRUE;
@@ -126,3 +102,7 @@ WHERE name = 'Sebrina';
 SELECT name FROM groupChat 
 WHERE isPublic = TRUE;
 
+SELECT Person.name FROM Person, groupChat, personGroupChatLink
+WHERE Person.ID = personGroupChatLink.personID
+AND groupChat.ID = personGroupChatLink.groupChatID
+AND groupChatID = 4
