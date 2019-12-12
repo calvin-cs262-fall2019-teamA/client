cd73031d500f89d73218a07ae240fede257808a2
hellenmakuei <ham8@students.calvin.edu>
2019-12-11 19:50:11 -0500

    profile pic code and terms and conditions

--- app/src/main/java/edu/calvin/cs262/healiva/Profile.java
+++ app/src/main/java/edu/calvin/cs262/healiva/Profile.java
@@ -1,9 +1,17 @@
 package edu.calvin.cs262.healiva;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +19,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
 
+import static android.Manifest.permission_group.CAMERA;
+
 /**
  * Profile displays profile setting for user
  * User can choose full anonymity which disable all other profile setting
  * If the user disables full anonymity they will be able to customize what PII information shows
  */
-public class Profile extends AppCompatActivity {
+public class Profile<activity_profile> extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 1 ;
+    private static final int CAMERA_CODE = 1;
     private Switch fullAnonymity;
     private CardView disabled;
     private EditText description;
@@ -39,17 +51,19 @@ public class Profile extends AppCompatActivity {
     /**
      * onCreateOptionsMenu should create the dropdown menu button in the top right corner of each page.
      * Options should include Profile, Settings, and Logout buttons
+     *
      * @param menu
      * @return true -- required return
      */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     /**
      * logout handles logout click and redirects to login page
+     *
      * @param item
      */
     public void logout(MenuItem item) {
@@ -59,6 +73,7 @@ public class Profile extends AppCompatActivity {
 
     /**
      * handleClickProfile should not do anything because we are already on the profile page
+     *
      * @param item
      */
     public void handleClickProfile(MenuItem item) {
@@ -67,6 +82,7 @@ public class Profile extends AppCompatActivity {
 
     /**
      * handleClickSettings handles settings click and redirects to settings page
+     *
      * @param item
      */
     public void handleClickSettings(MenuItem item) {
@@ -76,8 +92,9 @@ public class Profile extends AppCompatActivity {
 
     /**
      * handleFullAnonymity should
-     *      disable other profile settings buttons and make user fully anonymous if anonymity is changed to on
-     *      enable other profile settings buttons if anonymity is changed to off
+     * disable other profile settings buttons and make user fully anonymous if anonymity is changed to on
+     * enable other profile settings buttons if anonymity is changed to off
+     *
      * @param view
      */
     public void handleFullAnonymity(View view) {
@@ -85,7 +102,7 @@ public class Profile extends AppCompatActivity {
         // If full anonymity is checked
         // disable fields and make switches be off
         // Grey out all of screen but anonymity button
-        if(fullAnonymity.isChecked()){
+        if (fullAnonymity.isChecked()) {
             disabled.setVisibility(view.VISIBLE);
             description.setInputType(InputType.TYPE_NULL);
             showName.setClickable(false);
@@ -93,12 +110,48 @@ public class Profile extends AppCompatActivity {
             showName.setChecked(false);
             showPic.setChecked(false);
 
-        // enable fields and remove grey screen cover
+            // enable fields and remove grey screen cover
         } else {
             disabled.setVisibility(view.GONE);
             description.setInputType(InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE);
             showName.setClickable(true);
             showPic.setClickable(true);
         }
+    }
+
+
+    public void pickFile() {
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                CAMERA);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    getActivity(),
+                    new String[]{CAMERA},
+                    PERMISSION_CODE
+            );
+            return;
+        }
+        openCamera();
+    }
+
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_CODE);
+
     }
 }
